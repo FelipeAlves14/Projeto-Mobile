7eda01b9a0b9eb87e268930f5f3356ee72acd026
FelipeAlves14 <favasconcelos09@gmail.com>
2025-05-27 14:50:27 -0300

    feat: integra comportamento da tela com componente input

--- app/src/main/java/com/example/pratiler/AtualizarLeituraActivity.java
+++ app/src/main/java/com/example/pratiler/AtualizarLeituraActivity.java
@@ -1,27 +1,21 @@
 package com.example.pratiler;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.Spinner;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
+import java.util.Objects;
 
 public class AtualizarLeituraActivity extends AppCompatActivity {
 
@@ -44,6 +38,33 @@ public class AtualizarLeituraActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = new Intent("FEED");
                 startActivity(i);
+            }
+        });
+        Button postar = findViewById(R.id.botaoPostar);
+        postar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Input paginaInicial = findViewById(R.id.inputPaginaInicial);
+                Input paginaFinal = findViewById(R.id.inputPaginaFinal);
+                Input comentario = findViewById(R.id.inputComentario);
+                String valorPaginaInicial = Objects.requireNonNull(paginaInicial.getText()).toString();
+                String valorPaginaFinal = Objects.requireNonNull(paginaFinal.getText()).toString();
+                String valorComentario = Objects.requireNonNull(comentario.getText()).toString();
+                if (valorPaginaInicial.isEmpty()) {
+                    Toast.makeText(AtualizarLeituraActivity.this, "Insira a página inicial para poder postar...", LENGTH_SHORT).show();
+                }
+                else if (valorPaginaFinal.isEmpty()){
+                    Toast.makeText(AtualizarLeituraActivity.this, "Insira a página final para poder postar...", LENGTH_SHORT).show();
+                }
+                else if(Integer.parseInt(valorPaginaInicial) >= Integer.parseInt(valorPaginaFinal)){
+                    Toast.makeText(AtualizarLeituraActivity.this, "Insira a página final maior do que a página inicial", LENGTH_SHORT).show();
+                }
+                else if (valorComentario.isEmpty()){
+                    Toast.makeText(AtualizarLeituraActivity.this, "Insira o comentário para poder postar...", LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(AtualizarLeituraActivity.this, "Você realizou uma postagem!!", LENGTH_SHORT).show();
+                }
             }
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
